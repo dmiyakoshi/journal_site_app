d19e57ddcdf79c7038d23a6e60525f56216b4dfe
dmiyakoshi <d1m354dai@gmail.com>
2021-09-20 15:44:59 +0900

    showページのリンクがボタンになっていないので、ボタンに変更

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,21 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Show</title>
+    <link rel="stylesheetsgi" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <h1>論文詳細</h1>
     <p>タイトル:{{ $article->title }}</p>
     <p>{{ $article->body }}</p>
-    <div class="button">
-        <a href="/articles">一覧に戻る</a>
+    <div class="button-group">
+        {{-- <a href="/articles">一覧に戻る</a> --}}
+        <button type="button" onclick="location.href='/articles'">編集する</button>
+        {{-- <a href="/articles/{{ $article->id }}/edit">編集する</a> --}}
+        <button type="button" onclick="location.href='/articles/{{ $article->id }}/edit'">編集する</button>
+        <input type="submit" value="削除する" form="form-del">
+        <form action="/articles/{{ $article->id }}" method="post" id="form-del">
+            @csrf
+            @method('DELETE')
+        </form>
     </div>
-    <div class="button">
-        <a href="/articles/{{ $article->id }}/edit">編集する</a>
-    </div>
-    <input type="submit" value="削除する" form="form-del">
-    <form action="/articles/{{ $article->id }}" method="post" id="form-del">
-        @csrf
-        @method('DELETE')
-    </form>
 </body>
 </html>
