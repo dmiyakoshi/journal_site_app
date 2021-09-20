0dd9e4731b7e1b3643404fee31dec05a82587309
dmiyakoshi <d1m354dai@gmail.com>
2021-09-20 15:50:08 +0900

    スペルミスがあったかもしれないのでコミット

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Show</title>
-    <link rel="stylesheetsgi" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheets" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <h1>論文詳細</h1>
