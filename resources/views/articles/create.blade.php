d44fa48ed68f06b22dbf63d5c39b4bc7373c0c0f
dmiyakoshi <d1m354dai@gmail.com>
2021-09-21 10:11:42 +0900

    create.bladeとedit.bladeのインデントと@csrfの位置の修正 CSSのコメントアウトしていた不要なコードを削除しました

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -11,17 +11,17 @@
 <body>
     <h1>新規論文投稿</h1>
     <form action="/articles" method="POST">
-    <p>論文タイトル 
-        <br>
-        <input type="text" name="title" value="{{ old('title') }}">
-    </p>
-    <p>本文
-        <br>
-        <textarea name="body">
-            {{ old('body') }}
-        </textarea>
-    </p>
         @csrf
+        <p>論文タイトル 
+            <br>
+            <input type="text" name="title" value="{{ old('title') }}">
+        </p>
+        <p>本文
+            <br>
+            <textarea name="body">
+                {{ old('body') }}
+            </textarea>
+        </p>
         <input type="submit" value="投稿">
     </form>
 </body>
